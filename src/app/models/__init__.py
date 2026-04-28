570e6e14422f36ff9b102c4309578b179a81c69c
al1enn <wipetmp1404@proton.me>
2026-04-29 01:17:57 +0500

    add base model

---
+++ src/app/models/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ("Base",)
+from .base import Base
---
+++ src/app/models/mixins/__init__.py
@@ -0,0 +1,13 @@
+__all__ = (
+    "CreatedAtMixin",
+    "IntIdMixin",
+    "NameMixin",
+    "UpdatedAtMixin",
+    "UuidIdMixin",
+)
+
+from .created_at_mxin import CreatedAtMixin
+from .int_id_mixin import IntIdMixin
+from .name_mixin import NameMixin
+from .updated_at_mixin import UpdatedAtMixin
+from .uuid_id_mixin import UuidIdMixin
